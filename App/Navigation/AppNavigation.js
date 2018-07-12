99aef7c4cfb04909f751d43ffd5af432dc053439
flyw <flyw@163.com>
2018-07-12 16:18:17 +0800

    fix

--- App/Navigation/AppNavigation.js
+++ App/Navigation/AppNavigation.js
@@ -1,12 +1,10 @@
 import { StackNavigator } from 'react-navigation'
-import LoginScreen from '../Containers/LoginScreen'
 import LaunchScreen from '../Containers/LaunchScreen'
 
 import styles from './Styles/NavigationStyles'
 
 // Manifest of possible screens
 const PrimaryNav = StackNavigator({
-  LoginScreen: { screen: LoginScreen },
   LaunchScreen: { screen: LaunchScreen }
 }, {
   // Default config for all screens
